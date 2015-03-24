212747f9b59cc5903a8ecfb769e2ece19167be9c
alanzheng88 <alanzhengg@gmail.com>
2015-03-23 18:34:26 -0700

    remove drop table in migration - fix build

--- db/migrate/20150323083808_create_products.rb
+++ db/migrate/20150323083808_create_products.rb
@@ -1,6 +1,5 @@
 class CreateProducts < ActiveRecord::Migration
   def change
-    drop_table :products
     create_table :products do |t|
       t.string :name, limit: 30
       t.decimal :price, precision: 10, scale: 2
