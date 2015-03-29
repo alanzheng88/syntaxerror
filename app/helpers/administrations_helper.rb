611b113b0f324b513fae51c7957b0726f2b2e5ee
alanzheng88 <alanzhengg@gmail.com>
2015-03-29 12:00:18 -0700

    refactored helper script so that common code can be used in multiple views - satisfy DRY principle

--- app/helpers/administrations_helper.rb
+++ app/helpers/administrations_helper.rb
@@ -1,16 +1,3 @@
 module AdministrationsHelper
-
-	def draw_user_role_columns()
-		# concat(content_tag(:p, user_list))
-		tags = []
-		tags << content_tag(:thead) do
-		 	content_tag(:tr) do
-			    concat(content_tag(:th, 'Username'))
-			    concat(content_tag(:th, 'Firstname'))
-			    concat(content_tag(:th, 'Lastname'))
-			    concat(content_tag(:th, 'Role'))
-		 	end #content_tag :tr
-		end #content_tag :thead
-		tags.join.html_safe
-	end #draw_user_role_list
-end #AdministrationsHelper
+	include UsersHelper
+end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Syntaxerror
   class Application < Rails::Application
+    config.action_controller.include_all_helpers = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
